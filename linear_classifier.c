0a8a3747471d3f0894a5abb1174a49c1f97d13b7
modificaz <142904831+modificaz@users.noreply.github.com>
2024-05-01 16:47:12 +0000

    defined macro variables instead of magic numbers

--- linear_classifier.c
+++ linear_classifier.c
@@ -1,17 +1,25 @@
 /*                      LINEAR CLASSIFIER                         */
 /*															      */
 #define SAMPLES 200
+#define TRAINING_SAMPLES 160
+#define VALIDATION_SAMPLES 40
+#define MAX_EPOCHS 100
 #define INPUT_SIZE 5
+#define INPUT_SCL_FCT 1000
+#define OUTPT_SCL_FCT 2
+#define LEARNING_RT 0.5
+#define LEARNING_RT_DCR 0.0049
+#define OVRTRN_MAX_EPOCHS 5
 
 #include <math.h>
 #include <stdlib.h>
 
 double linearClassifier(int X[INPUT_SIZE], int data[][SAMPLES], int *labels)
 {
-	int iterations = 0;			 // number of training steps
-	double learning_rate = 0.5;	 // starting learning rate(reduced by 0.0049 every loop)
-	static int randomOrder[200]; // list of numbers 0-199 in random order
-	double weights[6];			 // perceptron weights
+	int epochs = 0;			 // number of training steps
+	double learning_rate = LEARNING_RT;	 // starting learning rate(reduced by LEARNING_RT_DCR every loop)
+	static int randomOrder[SAMPLES]; // list of numbers 0-199 in random order
+	double weights[INPUT_SIZE+1];// perceptron weights
 	int i, j;					 // counters
 	int training_correct = 0;	 // correct matches in training set
 	int validation_correct = 0;	 // correct matches in validation set
@@ -19,21 +27,21 @@ double linearClassifier(int X[INPUT_SIZE], int data[][SAMPLES], int *labels)
 	double fitnessValue;		 // value of fitness
 
 	/* choose random initial weights  */
-	for (i = 0; i < 6; i++)
+	for (i = 0; i < INPUT_SIZE+1; i++)
 	{
 		weights[i] = (double)rand() / RAND_MAX;
 	}
 
-	while (iterations < 100)
+	while (epochs < MAX_EPOCHS)
 	{
 
-		/* make list of numbers 0-199 in random order */
+		/* make list of numbers from 0 to (SAMPLES - 1) in random order */
 		int k;
-		for (k = 0; k < 200; k++)
+		for (k = 0; k < SAMPLES; k++)
 		{
 			randomOrder[k] = k;
 		}
-		for (k = 200 - 1; k >= 0; k--)
+		for (k = SAMPLES - 1; k >= 0; k--)
 		{
 			int j = rand() % (k + 1);
 			int temp = randomOrder[j];
@@ -44,7 +52,7 @@ double linearClassifier(int X[INPUT_SIZE], int data[][SAMPLES], int *labels)
 		training_correct = 0;
 		validation_correct = 0;
 		/* for all members of dataset(patients or healthy) */
-		for (i = 0; i < 200; i++)
+		for (i = 0; i < SAMPLES; i++)
 		{
 			/* check where they are categorized correctly */
 			int y;
@@ -60,11 +68,11 @@ double linearClassifier(int X[INPUT_SIZE], int data[][SAMPLES], int *labels)
 			if (y != labels[randomOrder[i]])
 			{
 				/* if is part of training set */
-				if (i < 160)
+				if (i < TRAINING_SAMPLES)
 				{
-					for (j = 0; j < 5; j++)
+					for (j = 0; j < INPUT_SIZE; j++)
 					{
-						weights[j] = weights[j] + (double)(learning_rate / 1000) * (double)(labels[randomOrder[i]] - y) * (double)data[X[j]][randomOrder[i]] / 2;
+						weights[j] = weights[j] + (double)(learning_rate / INPUT_SCL_FCT) * (double)(labels[randomOrder[i]] - y) * (double)data[X[j]][randomOrder[i]] / OUTPT_SCL_FCT;
 					}
 				}
 			}
@@ -72,7 +80,7 @@ double linearClassifier(int X[INPUT_SIZE], int data[][SAMPLES], int *labels)
 			else
 			{
 				/* if is part of training set */
-				if (i < 160)
+				if (i < TRAINING_SAMPLES)
 				{
 					training_correct++;
 				}
@@ -84,17 +92,17 @@ double linearClassifier(int X[INPUT_SIZE], int data[][SAMPLES], int *labels)
 			}
 		}
 
-		iterations++;
+		epochs++;
 		/* reduce learning rate */
-		learning_rate = learning_rate - 0.0049;
-		double mean_training_correct = (double)training_correct / 160;
-		double mean_validation_correct = (double)validation_correct / 40;
+		learning_rate -= LEARNING_RT_DCR;
+		double mean_training_correct = (double)training_correct / TRAINING_SAMPLES;
+		double mean_validation_correct = (double)validation_correct / VALIDATION_SAMPLES;
 		/* if for 5 turns training set is categorized better that validation */
 		/* then we have overtraining                                         */
 		if (mean_training_correct > mean_validation_correct)
 		{
 			overtraining++;
-			if (overtraining == 5)
+			if (overtraining == OVRTRN_MAX_EPOCHS)
 			{
 				break;
 			}
@@ -105,6 +113,6 @@ double linearClassifier(int X[INPUT_SIZE], int data[][SAMPLES], int *labels)
 		}
 	}
 	/* fitness funtion is: (total correctly categorized)/(total number) */
-	fitnessValue = ((double)training_correct + (double)validation_correct) / 200;
+	fitnessValue = ((double)training_correct + (double)validation_correct) / SAMPLES;
 	return fitnessValue;
 }
